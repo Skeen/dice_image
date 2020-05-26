ac44954bf02e13d0bf17f07337e38a507bb19da6
Emil Madsen <sovende@gmail.com>
2020-05-26 23:29:29 +0200

    Rework script w.r.t. borders

--- convert-picture.sh
+++ convert-picture.sh
@@ -1,54 +1,57 @@
 #!/bin/bash
 
-rm -rf output
-mkdir -p output
+################
+# Prepare dice #
+################
 
 # Create black dice from white dice
-for color in black; do
-    rm -rf dice_in/${color}/no_border
-    mkdir -p dice_in/${color}/no_border
-    find dice_in/${color}/no_border -name '*.png' -printf '%P\n' | xargs -I{} convert dice_in/${color}/no_border/{} -negate png24:dice_in/${color}/no_border/{}
-done
+rm -rf dice_in/black
+mkdir -p dice_in/black
+find dice_in/white -name '*.png' -printf '%P\n' | xargs -I{} convert dice_in/white/{} -negate png24:dice_in/black/{}
 
-# Create bordered dice from black and white dice
-for color in black white; do
-    rm -rf dice_in/${color}/border
-    mkdir -p dice_in/${color}/border
-    find dice_in/${color}/no_border -name '*.png' -printf '%P\n' | xargs -I{} convert dice_in/${color}/no_border/{} -fuzz 10% -fill ${color} -opaque "#FF0000" png24:dice_in/${color}/border/{}
-done
-
-for color in black white; do
-    for border in border no_border; do
-        rm -rf dice_out/${color}/${border}
-        mkdir -p dice_out/${color}/${border}
-        metapixel-prepare -width=15 -height=15 dice_in/${color}/${border} dice_out/${color}/${border}
-    done
-done
-
-exit
-# metapixel-prepare -width=15 -height=15 -recurse dice_in/ dice_out/black_and_white
-
-DICE_WIDTH=$1
-DICE_HEIGHT=$1
-
+######################
+# Prepare metapixels #
+######################
 PIXEL_WIDTH=15
 PIXEL_HEIGHT=15
+
+# Create metapixel sets for black and white
+for color in black white; do
+    rm -rf dice_out/${color}
+    mkdir -p dice_out/${color}
+    metapixel-prepare -width=15 -height=15 dice_in/${color} dice_out/${color}
+done
+
+# Create metapixel sets for mixed
+rm -rf dice_out/mixed
+mkdir -p dice_out/mixed
+metapixel-prepare -width=15 -height=15 dice_in --recurse dice_out/mixed
+
+#################
+# Prepare input #
+#################
+INPUT_IMAGE=$1
+
+DICE_WIDTH=$2
+DICE_HEIGHT=$2
 
 WIDTH=$(echo "${DICE_WIDTH} * ${PIXEL_WIDTH}" | bc)
 HEIGHT=$(echo "${DICE_HEIGHT} * ${PIXEL_HEIGHT}" | bc)
 
-convert input.png -resize ${WIDTH}x${HEIGHT} png24:output/original.png
+rm -rf output
+mkdir -p output
 
-convert input.png -resize ${DICE_WIDTH}x${DICE_HEIGHT} png24:input-tiny.png
-convert input-tiny.png -alpha off -colorspace sRGB +matte -scale 1500% -negate dpat_symbols.gif -virtual-pixel tile -fx 'u[floor(17.9999*u)+1]' output/magic.png
+convert ${INPUT_IMAGE} -resize ${WIDTH}x${HEIGHT} png24:output/original.png
+
+#################
+# Run metapixel #
+#################
 
 METAPIXEL_FLAGS="-i=0 -q=0 --distance=0 --width=15 --height=15"
+for color in black white mixed; do
+    for metric in wavelet subpixel; do
+        metapixel --library=dice_out/${color} --metapixel ${METAPIXEL_FLAGS} --metric=${metric} output/original.png output/${color}_${metric}.png
+        convert output/${color}_${metric}.png -background yellow -crop ${PIXEL_WIDTH}x0 -splice 1x0 +append +repage -crop 0x${PIXEL_HEIGHT} -splice 0x1 -append -gravity south -splice 0x1 -gravity east -splice 1x0 output/x_${color}_${metric}.png
+    done
+done
 
-metapixel --library=dice_out/white --metapixel ${METAPIXEL_FLAGS} --metric=wavelet output/original.png output/wave_white.png
-metapixel --library=dice_out/white --metapixel ${METAPIXEL_FLAGS} --metric=subpixel output/original.png output/subpixel_white.png
-
-metapixel --library=dice_out/black --metapixel ${METAPIXEL_FLAGS} --metric=wavelet output/original.png output/wave_black.png
-metapixel --library=dice_out/black --metapixel ${METAPIXEL_FLAGS} --metric=subpixel output/original.png output/subpixel_black.png
-
-metapixel --library=dice_out/black_and_white --metapixel ${METAPIXEL_FLAGS} --metric=wavelet output/original.png output/wave_black_and_white.png
-metapixel --library=dice_out/black_and_white --metapixel ${METAPIXEL_FLAGS} --metric=subpixel output/original.png output/subpixel_black_and_white.png
